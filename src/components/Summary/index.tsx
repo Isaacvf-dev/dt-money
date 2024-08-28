5310a4d97dbc58a4723735fb8bebb1424f5d8468
Isaac Francelino <isaacvf.dev@gmail.com>
2024-08-28 11:38:29 -0300

    Added data and price formatters

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { SummaryContainer, SummaryCard } from "./styles";
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export default function Summary() {
   const { transactions } = useContext(TransactionsContext)
@@ -31,7 +32,7 @@ export default function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income) }</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -39,7 +40,7 @@ export default function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
       <SummaryCard variant="green">
         <header>
